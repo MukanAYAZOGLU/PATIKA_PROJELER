ce443b4f72ea6e0b89577a819408e31c5fe05589
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-09-10 22:00:23 +0300

    java dosyasında ufak değişiklikler yapıldı.

--- JAVA 101/NotOrtalamasiHesaplayanProgram/src/NotOrtalamasiHesaplayanProgram.java
+++ JAVA 101/NotOrtalamasiHesaplayanProgram/src/NotOrtalamasiHesaplayanProgram.java
@@ -11,34 +11,34 @@ public class NotOrtalamasiHesaplayanProgram {
 
         int matNot, fizikNot, cogfNot, turkceNot,bedenNot, kimyaNot, geoNot;
 
-        System.out.print("Lütfen matematik notunu giriniz");
+        System.out.print("Lütfen matematik ders notunu giriniz: ");
         matNot=inp.nextInt();
 
-        System.out.print("Lütfen fizik notunu giriniz");
+        System.out.print("Lütfen fizik ders notunu giriniz: ");
         fizikNot=inp.nextInt();
 
-        System.out.print("Lütfen coğrafya notunu giriniz");
+        System.out.print("Lütfen coğrafya ders notunu giriniz: ");
         cogfNot=inp.nextInt();
 
-        System.out.print("Lütfen Türkçe notunu giriniz");
+        System.out.print("Lütfen Türkçe ders notunu giriniz: ");
         turkceNot=inp.nextInt();
 
-        System.out.print("Lütfen deben eğitimi notunu giriniz");
+        System.out.print("Lütfen beden eğitimi ders notunu giriniz: ");
         bedenNot=inp.nextInt();
 
-        System.out.print("Lütfen kimya notunu giriniz");
+        System.out.print("Lütfen kimya ders notunu giriniz: ");
         kimyaNot=inp.nextInt();
 
-        System.out.print("Lütfen geometri notunu giriniz");
+        System.out.print("Lütfen geometri ders notunu giriniz: ");
         geoNot=inp.nextInt();
 
         int toplam=(matNot+geoNot+turkceNot+fizikNot+bedenNot+cogfNot+kimyaNot);
 
         double ort=toplam/7;
 
-        System.out.println("Ders ortalamanız= "+ort);
+        System.out.println("Derslerinizin ortalaması= "+ort);
 
-        String gecKal=ort>=65 ? "Başarılı" : "Kaldı";
+        String gecKal=ort>=65 ? "Öğrenci durumu: Başarılı." : "Öğrenci durumu: Başarısız.";
 
         System.out.println(gecKal);
 
