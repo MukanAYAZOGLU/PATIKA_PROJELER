2a906d5bfdf1126c1dfa5bd2165459401825519c
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-09-12 12:06:28 +0300

    a açılı daire diliminin alanını hesaplama kodları eklendi.

--- JAVA 101/DaireninAlaniniVeCevresiniHesaplayanProgram/src/Main.java
+++ JAVA 101/DaireninAlaniniVeCevresiniHesaplayanProgram/src/Main.java
@@ -4,11 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         /*
+        1.Pratik
         r= yarı çap
         π= pi (3.14)
 
         Dairenin Çevre Formülü : 2 * π * r;
         Dairenin Alan Formülü : π * r * r;
+
+
+        2.Pratik
+        Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+        Formül : (𝜋 * (r*r) * 𝛼) / 360
          */
 
         Scanner inp= new Scanner(System.in);
@@ -16,15 +23,26 @@ public class Main {
         System.out.print("Dairenin yarı çapını giriniz: ");
         double r=inp.nextInt();
 
+        System.out.print("Merkez açısının değerini giriniz: ");
+        double aci=inp.nextInt();
+
+
         double pi=3.14;
         double cvr=2*pi*r;
         double aln=pi*Math.pow(r,2);
+        double dlmALan=(pi*Math.pow(r,2))*aci/360;
 
         System.out.println("Dairenin yarı çapı= "+r);
         System.out.println("Dairenin çevresi= "+cvr);
         System.out.println("Dairenin alanı= "+aln);
+        System.out.println(aci+" açılı daire diliminin alanı= "+dlmALan);
 
 
+        /*
+        ÖDEV
+
+
+         */
 
 
 
