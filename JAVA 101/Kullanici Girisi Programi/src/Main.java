ec35e470d9a25566bcaf1fc5b09840f24c2ad806
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-09-14 15:26:58 +0300

    3 hatalı giriş sonrası programın sonlandırılması için gerekli düzenleme yapıldı.

--- JAVA 101/Kullanici Girisi Programi/src/Main.java
+++ JAVA 101/Kullanici Girisi Programi/src/Main.java
@@ -6,10 +6,16 @@ public class Main {
         //Kullanıcı adımız: Patika
         //Kullanıcı e-postamız: patika-java@gmail.com
         //Kullanıcı şifremiz: Java123
+        //Kullanıcı şifreyi yenilemek için 3 hakkı var.
 
         String userName = "Java", userEMail = "patika-java@gmail.com", userPassword = "Java123";
         String userEntr1;
         String userEntr2;
+        String newPassword;
+        String newPasswordContrl;
+        int mistake=1;
+
+        char answr;
 
         Scanner inp = new Scanner(System.in);
 
@@ -26,6 +32,67 @@ public class Main {
                         System.out.println("Giriş başarılı.");
 
                     } else System.out.println("Şifreniz hatalı.");
+
+                    if (!(userEntr2.equals(userPassword))) {
+
+                        System.out.print("Şifreniz hatalı. Şifrenizi sıfırlamak ister misiniz (E/h): ");
+
+                        answr=inp.next().charAt(0);
+
+                        switch (answr) {
+
+                            case 'E':
+
+                                System.out.print("Yeni şifrenizi giriniz: ");
+                                newPassword=inp.next();
+
+                                while(newPassword.equals(userPassword) && mistake<3) {
+
+                                    System.out.println("Şifreniz oluşturulamadı, lütfen başka şifre giriniz.");
+
+                                    System.out.print("Yeni şifrenizi giriniz: ");
+                                    newPassword=inp.next();
+
+                                    mistake++;
+
+
+                                if (mistake==3) {
+
+                                System.out.println("3 kez hatalı giriş gerçekleştirdiniz. Şifre yenileme işleminiz " +
+                                        "iptal" +
+                                        " edilmiştir.");
+
+                                }
+
+                                }
+
+
+
+                                if (!(newPassword.equals(userPassword))) {
+
+                                    System.out.print("Yeni şifrenizi tekrar giriniz: ");
+                                    newPasswordContrl = inp.next();
+
+                                    if (newPassword.equals(newPasswordContrl)) {
+
+                                        System.out.println("Şifreniz oluşturuldu.");
+                                    }
+                                }
+
+                                break;
+
+                            case 'h':
+                                System.out.println("Program sonlandırıldı.");
+                                break;
+
+                            default:
+                                System.out.println("Hatalı tuşlama yaptınız.");
+
+
+                        }
+
+
+                    }
 
 
             } else System.out.println("Kullanıcı adınız veya e-postanız hatalı.");
