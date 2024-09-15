4d6198a113c111329f267e45fe08107b3b073efd
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-09-15 22:46:51 +0300

    Main.java dosyasında ikinci program yazıldı.

--- JAVA 101/Tek Sayilarin Toplamini Bulan Program/src/Main.java
+++ JAVA 101/Tek Sayilarin Toplamini Bulan Program/src/Main.java
@@ -5,9 +5,12 @@ public class Main {
 
     //Negatif bir değer girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden tek sayıları toplayıp ekrana basan programı yazıyoruz.
 
-        int num;
+        int num, num2;
         int howMany=0;
         int total=0;
+
+        int howMany2=0;
+        int total2=0;
 
 
         Scanner inp = new Scanner(System.in);
@@ -30,6 +33,38 @@ public class Main {
 
         System.out.println("Toplamda "+howMany+" adet tek sayı girdiniz.");
         System.out.println("Girmiş olduğunuz tek sayıların toplamı= "+total);
+        System.out.println("Tek sayıların toplamını bulan program sonlandı.");
+
+
+        /*
+          Tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+        */
+
+        System.out.println("-------------------");
+
+
+        do{
+
+            System.out.print("Lütfen sayınızı giriniz: ");
+            num2=inp.nextInt();
+
+            if ((num2%2==0) && (num2%4==0)) {
+
+                System.out.println("2 ve 4'ün katı olan rakam: "+num2);
+                total2+=num2;
+
+                howMany2++;
+
+            }
+
+        }while(num2%2==0);
+
+        System.out.println("2 ve 4'ün katı olan toplamda "+howMany2+" adet sayı girdiniz.");
+        System.out.println("2 ve 4'ün katı olan rakamlarınızın toplamı= "+total2);
+        System.out.println("2 ve 4'ün katları olan sayıların toplamını bulan program sonlandı.");
+
+
+
 
     }
 }
