ecd3780ad0e6cab9f1f4c39910f1b91eca046eb2
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-09-19 18:24:53 +0300

    ATM Projesi klasörleri yüklendi.

---
+++ JAVA 101/ATM Projesi/src/Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String userName;
+        String password;
+        int right=3;
+        int balance = 1500;
+        int choice;
+        int amount;
+
+        Scanner input = new Scanner(System.in);
+
+
+        do{
+
+            System.out.print("Kullancı adınız: ");
+            userName= input.nextLine();
+
+            System.out.print("Parolanız: ");
+            password= input.nextLine();
+
+            if (userName.equals("Patika") && password.equals("Java101")) {
+
+                System.out.println("Merhaba Efendim, Patika Bankasına hoş geldiniz.");
+
+                do {
+
+                    System.out.println(
+                        "1 - Para yatırma\n"+
+                        "2 - Para çekme\n"+
+                        "3 - Bakiye sorgulama\n"+
+                        "4 - Çıkış yap");
+
+
+                    System.out.print("Lütfen yapmak istediğini işlemi belirtiniz: ");
+                    choice=input.nextInt();
+
+                    if (choice==1) {
+
+                        System.out.print("Para miktarı: ");
+                        amount=input.nextInt();
+                        balance+=amount;
+
+                    }
+
+                    else if (choice==2) {
+
+                        System.out.print("Para miktarı: ");
+                        amount=input.nextInt();
+                        if (amount<=balance) {
+                            balance-=amount;
+
+                        }
+                        else System.out.println("Bakiye yetersiz.");
+
+                    }
+
+                    else if (choice==3) {
+                        System.out.println("Bakiyeniz: "+balance);
+
+                    }
+
+                }while(choice!=4);
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+
+
+            }else {
+                right--;
+                if (right>0) {
+                    System.out.println("Kullanıcı adı veya parola yanlış. Tekrar deneyiniz.");
+                    System.out.println(right+" giriş hakkınız kaldı.");
+
+                } else {
+                    System.out.println("Kullanıcı adı veya parola yanlış.");
+                    System.out.println("Hesabınız bloke olmuştur. Lütfen banka ile iletişime geçiniz.");
+                }
+            }
+
+        }while (right>0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
