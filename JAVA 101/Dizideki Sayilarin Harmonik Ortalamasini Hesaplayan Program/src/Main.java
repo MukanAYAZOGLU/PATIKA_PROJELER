c51b12e050e2932cf25fb765d45f4601e50c5ee3
Mukan AYAZOĞLU <postaturkey@yaani.com>
2024-10-04 00:19:41 +0300

    Dizideki Sayilarin Harmonik Ortalamasini Hesaplayan Program directories pushed.

---
+++ JAVA 101/Dizideki Sayilarin Harmonik Ortalamasini Hesaplayan Program/src/Main.java
@@ -0,0 +1,53 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOfElements;
+        double harmonicSeries=0;
+
+        Scanner inp = new Scanner(System.in);
+
+
+        System.out.print("Dizinizde kaç eleman olmasını istersiniz: ");
+        numberOfElements=inp.nextInt();
+
+        int [] myArray= new int[numberOfElements];
+
+
+        System.out.println("Dizi elemanlarınızı giriniz: ");
+
+        for (int i = 0; i <numberOfElements ; i++) {
+
+            myArray[i]=inp.nextInt();
+        }
+
+        System.out.print("Dizi elemanlarınız: ");
+
+        for (int a :myArray ) {
+            System.out.print(a+" ");
+
+        }
+
+
+        //1+(1/2)+(1/3)+(1/4)+(1/5)...+(1/N)
+
+        for (int i = 1; i <= myArray.length ; i++) {
+
+            harmonicSeries+= (double) 1 /i;
+            
+        }
+
+        System.out.println();
+        System.out.println(myArray.length);
+        System.out.println(harmonicSeries);
+
+        System.out.println("Dizi elemanlarınızın harmonik ortalaması = "+new DecimalFormat("##.##").format(((double) myArray.length)/harmonicSeries));
+
+
+
+
+
+    }
+}
